a0243b8f87ea1911f2aa59c097e316305574cc0a
Kyle Holgate <kyle.holgate@gmail.com>
2014-08-25 15:14:25 -0400

    fixed case-sensitive issue on search

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ActiveRecord::Base
   scope :companies, lambda{|company_names| where("company_name IN (?)", company_names )}
-  scope :food_name, lambda{|food_name| where('food_name LIKE ?', '%'+food_name+'%' )}
+  scope :food_name, lambda{|food_name| where('lower(food_name) LIKE ?', '%'+food_name.downcase+'%' )}
   scope :protein, lambda{|protein| where('protein >= ?', protein )}
   scope :cals, lambda{|cals| where('calories <= ?', cals )}
   
